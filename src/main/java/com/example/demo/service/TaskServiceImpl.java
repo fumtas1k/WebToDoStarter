92750c6310f4ccde27d3b05a89948c19f53afe9b
fumtas1k <fumta01a7@gmail.com>
2022-08-16 11:41:43 +0900

    add:service

--- src/main/java/com/example/demo/service/TaskServiceImpl.java
+++ src/main/java/com/example/demo/service/TaskServiceImpl.java
@@ -1,18 +1,22 @@
 package com.example.demo.service;
 
+import com.example.demo.dao.TaskDao;
+import com.example.demo.entity.Task;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
 import java.util.List;
 import java.util.Optional;
 
-import org.springframework.stereotype.Service;
-
-import com.example.demo.dao.TaskDao;
-import com.example.demo.entity.Task;
-
 @Service
+@Transactional
 public class TaskServiceImpl implements TaskService {
 
 	private final TaskDao dao;
 
+	@Autowired
 	public TaskServiceImpl(TaskDao dao) {
 		this.dao = dao;
 	}
@@ -25,12 +29,12 @@ public class TaskServiceImpl implements TaskService {
 	@Override
 	public Optional<Task> getTask(int id) {
 
-		//削除してください
-		Optional<Task> taskOpt = null;
-		return taskOpt;
-
 		//Optional<Task>一件を取得 idが無ければ例外発生　
-
+		try {
+			return dao.findById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new TaskNotFoundException("指定されたタスクが存在しません");
+		}
 	}
 
 	@Override
@@ -42,14 +46,18 @@ public class TaskServiceImpl implements TaskService {
 	public void update(Task task) {
 
 		//Taskを更新　idが無ければ例外発生
-
+		if (dao.update(task) == 0) {
+			throw new TaskNotFoundException("更新するタスクが存在しません");
+		}
 	}
 
 	@Override
 	public void deleteById(int id) {
 
 		//Taskを更新 idがなければ例外発生
-
+		if (dao.deleteById(id) == 0) {
+			throw new TaskNotFoundException("削除するタスクが存在しません");
+		}
 	}
 
 	@Override
